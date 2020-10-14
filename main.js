b33668cade8ff947e303f20afbeee8c504e9a89a
Toriel Oreginau <72525388+TorielOreginal@users.noreply.github.com>
2020-10-14 09:36:45 -0300

    Updated by Foxy

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-const audio = document.getElementById('audio')
-const date = new Date()
-
-const hours = date.getHours()
-const minutes = date.getMinutes()
-const seconds = date.getSeconds()
-
-if(hours === 0 && minutes === 0){
-    audio.currentTime = 0
-    audio.play()
-}
-
 function clock(){
+    const audio = document.getElementById('audio')
     const clockText = document.getElementById('clock')
     const date = new Date()
 
@@ -18,16 +7,35 @@ function clock(){
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
-    const hoursLeft = hours - 24
-    const minutesLeft = minutes - 60
-    const secondsLeft = seconds - 60
+    const hoursLeft = String(-(hours - 23))
+    const minutesLeft = String(-(minutes - 60))
+    const secondsLeft = String(-(seconds - 60))
 
-    document.title = `${-hoursLeft}:${-minutesLeft}:${-secondsLeft} restantes`
-    clockText.innerText = `${-hoursLeft}:${-minutesLeft}:${-secondsLeft}`
+    function updateTime(k) {
+        if (parseInt(k) < 10) {
+            return "0" + k;
+        }
+        else {
+            return k;
+        }
+    }
+
+    const hoursLeft1 = updateTime(hoursLeft)
+    const minutesLeft1 = updateTime(minutesLeft)
+    const secondsLeft1 = updateTime(secondsLeft)
+
+    document.title = `${hoursLeft1}:${minutesLeft1}:${secondsLeft1} restantes`
+    clockText.innerText = `${hoursLeft1}:${minutesLeft1}:${secondsLeft1}`
     if(hours === 0 && minutes === 0){
         document.title = "MEIA NOITE!"
         clockText.innerText = "Meia-noite!"
     }
+
+    if(hours === 0 && minutes === 0 && seconds === 0){
+        audio.currentTime = 0
+        audio.play()
+    }
+
 }
 
 setInterval(clock, 1000)
